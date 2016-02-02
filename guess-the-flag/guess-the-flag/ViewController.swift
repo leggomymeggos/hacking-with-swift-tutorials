aa954ba6881bfd2b98c3fdaf9945be34e36e6b62
Megan Gaebler <mgaebler@pivotal.io>
2016-02-02 08:23:59 -0600

    WIP Project 2: Guess the Flag

---
+++ guess-the-flag/guess-the-flag/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  guess-the-flag
+//
+//  Created by pivotal on 2/1/16.
+//  Copyright © 2016 pivotal. All rights reserved.
+//
+
+import GameplayKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+
+        button1.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button2.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button3.layer.borderColor = UIColor.lightGrayColor().CGColor
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        askQuestion()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func askQuestion() {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+    }
+
+}
